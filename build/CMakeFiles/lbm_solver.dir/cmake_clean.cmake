32feac5dca2e2ffe7f5e45a4388f778bf90f0396
Yung-Jiun Chen <skyw4lkzz@gmail.com>
2026-05-26 23:18:02 +0800

    Add files via upload

---
+++ build/CMakeFiles/lbm_solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lbm_solver.dir/src/main.cpp.o"
+  "CMakeFiles/lbm_solver.dir/src/main.cpp.o.d"
+  "lbm_solver"
+  "lbm_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lbm_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
